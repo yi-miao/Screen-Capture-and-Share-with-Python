f474dab4e7c3dc43f81d667f85439f87658a6a66
Yi Miao <40175039+yi-miao@users.noreply.github.com>
2022-11-22 15:31:47 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ def Streaming(name, sock):
 			data = data[msg_size:]
 
 			frame = pickle.loads(frame_data)
-			cv2.imshow("Streaming ",frame)
+			cv2.imshow("Screenshot ",frame)
 			cv2.waitKey(1)
 		except:
 			cv2.destroyAllWindows()
